7f5f909cf416e96342ffc22ce2c7daf4d3667774
oylenshpeegul <theaney@gmail.com>
2016-11-05 14:13:08 -0400

    mozilla, symbian, unix

--- src/lib.rs
+++ src/lib.rs
@@ -20,10 +20,16 @@ pub fn java (num: i64) -> NaiveDateTime {
 pub fn mozilla (num: i64) -> NaiveDateTime {
 	epoch2time(num, 1_000_000, 0)
 }
+pub fn symbian (num: i64) -> NaiveDateTime {
+    epoch2time(num, 1_000_000, -62_167_219_200)
+}
+pub fn unix (num: i64) -> NaiveDateTime {
+    epoch2time(num, 1, 0)
+}
 
 #[cfg(test)]
 mod tests {
-    use super::{chrome, cocoa, java, mozilla};
+    use super::{chrome, cocoa, java, mozilla, symbian, unix};
     
     #[test]
     fn chrome_run() {
@@ -54,5 +60,23 @@ mod tests {
         let ndt = mozilla(1234567890000000);
         assert_eq!(ndt.to_string(), "2009-02-13 23:31:30");
     }
+
+    #[test]
+    fn symbian_run() {
+        let ndt = symbian(63401787090000000);
+        assert_eq!(ndt.to_string(), "2009-02-13 23:31:30");
+    }
+
+    #[test]
+    fn unix_run() {
+        let ndt = unix(1234567890);
+        assert_eq!(ndt.to_string(), "2009-02-13 23:31:30");
+    }
+    #[test]
+    fn unix_minus_run() {
+        let ndt = unix(-1234567890);
+        assert_eq!(ndt.to_string(), "1930-11-18 00:28:30");
+    }
+
 }
 
