82cd9c151938c0ded74cc61d2a0288b77d629c4c
oylenshpeegul <theaney@gmail.com>
2016-11-05 15:02:57 -0400

    windows_date, windows_file

--- src/lib.rs
+++ src/lib.rs
@@ -26,10 +26,21 @@ pub fn symbian (num: i64) -> NaiveDateTime {
 pub fn unix (num: i64) -> NaiveDateTime {
     epoch2time(num, 1, 0)
 }
-
+pub fn uuid_v1 (num: i64) -> NaiveDateTime {
+	epoch2time(num, 10_000_000, -12_219_292_800)
+}
+pub fn windows_date (num: i64) -> NaiveDateTime {
+    epoch2time(num, 10_000_000, -62_135_596_800)
+}
+pub fn windows_file (num: i64) -> NaiveDateTime {
+    epoch2time(num, 10_000_000, -11_644_473_600)
+}
+    
 #[cfg(test)]
 mod tests {
-    use super::{chrome, cocoa, java, mozilla, symbian, unix};
+
+    use super::{chrome, cocoa, java, mozilla, symbian, unix, uuid_v1,
+    windows_date, windows_file};
     
     #[test]
     fn chrome_run() {
@@ -78,5 +89,38 @@ mod tests {
         assert_eq!(ndt.to_string(), "1930-11-18 00:28:30");
     }
 
+    #[test]
+    fn uuid_run() {
+        let ndt = uuid_v1(134538606900000000);
+        assert_eq!(ndt.to_string(), "2009-02-13 23:31:30");
+    }
+    #[test]
+    fn uuid_micros() {
+        let ndt = uuid_v1(0x1dc7711a73088f5);
+        assert_eq!(ndt.to_string(), "2007-10-10 09:17:41.739749300");
+    }
+
+    #[test]
+    fn windows_date_run() {
+        let ndt = windows_date(633701646900000000);
+        assert_eq!(ndt.to_string(), "2009-02-13 23:31:30");
+    }
+    #[test]
+    fn windows_date_micros() {
+        let ndt = windows_date(634496538123456789);
+        assert_eq!(ndt.to_string(), "2011-08-22 23:50:12.345678900");
+    }
+
+    #[test]
+    fn windows_file_run() {
+        let ndt = windows_file(128790414900000000);
+        assert_eq!(ndt.to_string(), "2009-02-13 23:31:30");
+    }
+    #[test]
+    fn windows_file_micros() {
+        let ndt = windows_file(0x1cabbaa00ca9000);
+        assert_eq!(ndt.to_string(), "2010-03-04 14:50:16.559001600");
+    }
+    
 }
 
